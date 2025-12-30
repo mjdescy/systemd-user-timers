f7994102fb91a71dd437a46db78bffad34a0e1da
Michael Descy <mjdescy@users.noreply.github.com>
2025-12-30 22:19:58 +0000

    Started to add a subcommand for "add"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Command, arg, command, value_parser};
+
 fn main() {
-    println!("Hello, world!");
+
+    let matches = command!()
+    .subcommand(
+        Command::new("add")
+            .about("Add a timer")
+            .args([
+                arg!(-e --exec <EXECUTABLE> "The executable the timer will run")
+                    .value_parser(value_parser!(String)),
+                arg!(-m --"exec-if-missed" "Execute immediately if missed")
+                    .action(clap::ArgAction::SetTrue)
+            ])
+    )
+    .get_matches();
+
+    if let Some(add_matches) = matches.subcommand_matches("add") {
+        println!("Add subcommand exec-if-missed value: {}", add_matches.get_flag("exec-if-missed"));
+    } else {
+        println!("No subcommand")
+    }
 }
